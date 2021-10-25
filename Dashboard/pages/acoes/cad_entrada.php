249a9ebcb94dffb2f2971a617b4eef527c03e03a
Ryan Nicolau Lopes <61668167+Ryan-NLopes@users.noreply.github.com>
2021-10-25 09:45:28 -0300

    Corrigindo Bug da Listagem

---
+++ Dashboard/pages/acoes/cad_entrada.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+include_once("conexao2.php");
+
+$valor =  $_POST['valor'];
+$descricao = $_POST['descricao'];
+
+
+	$query = "INSERT INTO entrada(id, descricao, valor) 
+	VALUES (default,  '$descricao','$valor')";
+	$busca = mysqli_query($conexao, $query);
+
+	$_SESSION['msg_cad'] = "<p style='color:green; font-weight: bold;'>USUÁRIO CADASTRADO COM SUCESSO!</p>";
+	header("Location: ../gastos.php");
--- Dashboard/pages/gastos.php
+++ Dashboard/pages/gastos.php
@@ -37,10 +37,10 @@
 </head>
 
 <body class="g-sidenav-show  bg-gray-100">
-<?php include "navbar.php";?>
+  <?php include "navbar.php"; ?>
   <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ">
-      <!-- Navbar -->
-      <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+    <!-- Navbar -->
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
@@ -52,24 +52,24 @@
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
             <ul class="navbar-nav  justify-content-end">
-            <li class="nav-item d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Sair</span>
-              </a>
-            </li>
-            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-                <div class="sidenav-toggler-inner">
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                </div>
-              </a>
-            </li>
-          </ul>
+              <li class="nav-item d-flex align-items-center">
+                <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
+                  <i class="fa fa-user me-sm-1"></i>
+                  <span class="d-sm-inline d-none">Sair</span>
+                </a>
+              </li>
+              <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                  <div class="sidenav-toggler-inner">
+                    <i class="sidenav-toggler-line"></i>
+                    <i class="sidenav-toggler-line"></i>
+                    <i class="sidenav-toggler-line"></i>
+                  </div>
+                </a>
+              </li>
+            </ul>
           </div>
-          
+
         </div>
       </div>
     </nav>
@@ -112,120 +112,121 @@
             </div>
           </div>
         </div>
-      <div class="row">
-      <div class="col-md-6 mt-4">
-          <div class="card h-100 mb-4">
-            <div class="card-header pb-0 px-3">
-              <div class="row">
-                <div class="col-md-6">
-                  <h6 class="mb-0">Entrada</h6>
+        <div class="row">
+          <div class="col-md-6 mt-4">
+            <div class="card h-100 mb-4">
+              <div class="card-header pb-0 px-3">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h6 class="mb-0">Entrada</h6>
+                  </div>
+                  <div class="col-md-3">
+                    <a class="btn" href="cad_entrada.php">Cadastrar</a>
+                  </div>
                 </div>
-                <div class="col-md-3">
-                <a class="btn" href="cad_despesa.html">Cadastrar</a>
               </div>
+              <?php
+              require('conexao.php');
+
+              $query = "SELECT * FROM entrada";
+              $busca = mysqli_query($conexao, $query);
+
+              while ($dados = mysqli_fetch_array($busca)) {
+                $id = $dados['id'];
+
+              ?>
+              <div class="card-body pt-4 p-3">
+                <ul class="list-group">
+                  <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                    <div class="d-flex align-items-center">
+                      <button class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-up"></i></button>
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-1 text-dark text-sm"><?php echo $dados['descricao']; ?></h6>
+                      </div>
+                    </div>
+                    <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
+                    +R$<?php echo $dados['valor']; ?>
+                    </div>
+                  </li>
+                </ul>
+                <?php } ?>
               </div>
             </div>
-            <div class="card-body pt-4 p-3">
-              <ul class="list-group">
-                <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                  <div class="d-flex align-items-center">
-                    <button class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-up"></i></button>
-                    <div class="d-flex flex-column">
-                      <h6 class="mb-1 text-dark text-sm">Intituto Adventista de Ensino</h6>
+          </div>
+          <div class="col-md-6 mt-4">
+            <div class="card h-100 mb-4">
+              <div class="card-header pb-0 px-3">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h6 class="mb-0">Saida</h6>
+                  </div>
+                  <div class="col-md-3">
+                    <a class="btn" href="cad_despesa.php">Cadastrar</a>
+                  </div>
+                </div>
+              </div>
+              <?php
+              require('conexao.php');
+
+              $query = "SELECT * FROM gastos";
+              $busca = mysqli_query($conexao, $query);
+
+              while ($dados = mysqli_fetch_array($busca)) {
+                $id = $dados['id'];
+
+              ?>
+              <div class="card-body pt-4 p-3">
+                <ul class="list-group">
+                  <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                    <div class="d-flex align-items-center">
+                      <button class="btn btn-icon-only btn-rounded btn-outline-danger mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-down"></i></button>
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-1 text-dark text-sm"><?php echo $dados['descricao']; ?></h6>
+                      </div>
                     </div>
-                  </div>
-                  <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                    + R$ 2000.00
-                  </div>
-                </li>
-              </ul>
-      
+                    <div class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
+                    -R$<?php echo $dados['valor']; ?>
+                    </div>
+                  </li>
+                </ul>
+                <?php } ?>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-md-6 mt-4">
-          <div class="card h-100 mb-4">
-            <div class="card-header pb-0 px-3">
-              <div class="row">
-                <div class="col-md-6">
-                  <h6 class="mb-0">Saidas</h6> 
+        <footer class="footer pt-3  ">
+          <div class="container-fluid">
+            <div class="row align-items-center justify-content-lg-between">
+              <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                  © <script>
+                    document.write(new Date().getFullYear())
+                  </script>,
+                  made with <i class="fa fa-heart"></i> by
+                  <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+                  for a better web.
                 </div>
-              <div class="col-md-3">
-              <a class="btn" href="cad_despesa.php">Cadastrar</a>
               </div>
-                
+              <div class="col-lg-6">
+                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                  <li class="nav-item">
+                    <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                  </li>
+                </ul>
               </div>
-            </div>
-        
-            <div class="card-body pt-4 p-3">
-              <ul class="list-group">
-                <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                  <div class="d-flex align-items-center">
-                    <button class="btn btn-icon-only btn-rounded btn-outline-danger mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i class="fas fa-arrow-down"></i></button>
-                    <div class="d-flex flex-column">
-                    <?php
-                          require('conexao.php');
-                          $sql = 'SELECT * FROM despesas';
-                          if($res=mysqli_query($conexao, $sql)){
-                          $descricao = array();
-                          $valor = array();
-                          $i = 0;
-
-                          while ($reg=mysqli_fetch_assoc($res)){
-                          $descricao[$i] = $reg['descricao'];
-                          $valor[$i] = $reg['valor'];
-                  ?> 
-                  <h6 class="mb-1 text-dark text-sm"><?php echo $descricao[$i]; ?></h6>
-                    </div>
-                  </div>
-                  <div class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
-                  <?php echo $dados['valor'] ?>
-                  </div>
-                  <?php
-                  $i++;
-                          }
-                        }
-
-                    ?>
-                </li>
-              </ul>
             </div>
           </div>
-        </div>
+        </footer>
       </div>
-      <footer class="footer pt-3  ">
-        <div class="container-fluid">
-          <div class="row align-items-center justify-content-lg-between">
-            <div class="col-lg-6 mb-lg-0 mb-4">
-              <div class="copyright text-center text-sm text-muted text-lg-start">
-                © <script>
-                  document.write(new Date().getFullYear())
-                </script>,
-                made with <i class="fa fa-heart"></i> by
-                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                for a better web.
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>
   </main>
   <div class="fixed-plugin">
     <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
