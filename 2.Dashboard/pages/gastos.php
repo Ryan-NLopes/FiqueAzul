d248d117d22c5e1d26432165f5250adf4a6ed5f7
Ryan Nicolau Lopes <61668167+Ryan-NLopes@users.noreply.github.com>
2021-10-27 09:54:30 -0300

    Atualização

--- 2.Dashboard/pages/gastos.php
+++ 2.Dashboard/pages/gastos.php
@@ -78,97 +78,70 @@
     </nav>
     <!-- End Navbar -->
     <div class="container-fluid py-4">
-      <div class="row">
-        <div class="col-lg-8">
+      <div class="col-lg-8">
+        <div class="col-xl-6">
           <div class="row">
-            <div class="col-xl-6">
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="card">
-                    <div class="card-header mx-4 p-3 text-center">
-                      <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
-                      <i class="bi bi-cash-coin"></i>
-                      </div>
-                    </div>
-                    <?php
-                        require('conexao.php');
-                        $soma = "SELECT SUM(valor) AS VALOR FROM entrada";
-                        $resultado = mysqli_query($conexao, $soma);
-                        $entrada = mysqli_fetch_assoc($resultado);
-                        ?>
-                    <div class="card-body pt-0 p-3 text-center">
-                      <h6 class="text-center mb-0">Total Entrada</h6>
-                      <hr class="horizontal dark my-3">
-                      <h5 class="mb-0">R$<?php echo number_format((float)$entrada['VALOR'], 2, ',', '.'); ?></h5>
-                    </div>
+            <div class="col-md-6">
+              <div class="card">
+                <div class="card-header mx-4 p-3 text-center">
+                  <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
+                    <i class="bi bi-cash-coin"></i>
                   </div>
                 </div>
-                
-                <div class="col-md-6 mt-md-0 mt-4">
-                  <div class="card">
-                    <div class="card-header mx-4 p-3 text-center">
-                      <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
-                      <i class="bi bi-graph-down-arrow"></i>
-                      </div>
-                    </div>
-                    <?php
-                        require('conexao.php');
-                        $soma = "SELECT SUM(valor) AS VALOR FROM gastos";
-                        $resultado = mysqli_query($conexao, $soma);
-                        $gastos = mysqli_fetch_assoc($resultado);
-                        ?>
-                    <div class="card-body pt-0 p-3 text-center">
-                      <h6 class="text-center mb-0">Total Despesas</h6>
-                      <hr class="horizontal dark my-3">
-                      <h5 class="mb-0">R$<?php echo number_format((float)$gastos['VALOR'], 2, ',', '.'); ?></h5>
-                    </div>
-                  </div>
+                <?php
+                require('conexao.php');
+                $soma = "SELECT SUM(valor) AS VALOR FROM entrada";
+                $resultado = mysqli_query($conexao, $soma);
+                $entrada = mysqli_fetch_assoc($resultado);
+                ?>
+                <div class="card-body pt-0 p-3 text-center">
+                  <h6 class="text-center mb-0">Total Entrada</h6>
+                  <hr class="horizontal dark my-3">
+                  <h5 class="mb-0">R$<?php echo number_format((float)$entrada['VALOR'], 2, ',', '.'); ?></h5>
                 </div>
-                <div class="col-md-6 mt-md-0 mt-4">
-                  <div class="card">
-                    <div class="card-header mx-4 p-3 text-center">
-                      <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
-                      <i class="bi bi-cash-coin"></i>
-                      </div>
-                    </div>
-                    <?php
-                        require('conexao.php');
-                        $soma = "SELECT SUM(valor) AS VALORSAIDA FROM gastos";
-                        $resultado = mysqli_query($conexao, $soma);
-                        $gastos = mysqli_fetch_assoc($resultado);
-
-                        $soma = "SELECT SUM(valor) AS VALOR FROM entrada";
-                        $resultado = mysqli_query($conexao, $soma);
-                        $entrada = mysqli_fetch_assoc($resultado);
-
-                        $resultado = $entrada['VALOR'] - $gastos['VALORSAIDA'];
-                        ?>
-                    <div class="card-body pt-0 p-3 text-center">
-                      <h6 class="text-center mb-0">Saldo Disponivel</h6>
-                      <hr class="horizontal dark my-3">
-                      <h5 class="mb-0">R$<?php echo number_format((float)$resultado, 2, ',', '.'); ?></h5>
-                    </div>
-                  </div>
-                </div>
-                
               </div>
             </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 mt-4">
-            <div class="card h-100 mb-4">
-              <div class="card-header pb-0 px-3">
-                <div class="row">
-                  <div class="col-md-6">
-                    <h6 class="mb-0">Entrada</h6>
-                  </div>
-                  <div class="col-md-3">
-                    <a class="btn" href="cad_entrada.php">Cadastrar</a>
+
+            <div class="col-md-6 ">
+              <div class="card">
+                <div class="card-header mx-4 p-3 text-center">
+                  <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
+                    <i class="bi bi-graph-down-arrow"></i>
                   </div>
                 </div>
+                <?php
+                require('conexao.php');
+                $soma = "SELECT SUM(valor) AS VALOR FROM gastos";
+                $resultado = mysqli_query($conexao, $soma);
+                $gastos = mysqli_fetch_assoc($resultado);
+                ?>
+                <div class="card-body pt-0 p-3 text-center">
+                  <h6 class="text-center mb-0">Total Despesas</h6>
+                  <hr class="horizontal dark my-3">
+                  <h5 class="mb-0">R$<?php echo number_format((float)$gastos['VALOR'], 2, ',', '.'); ?></h5>
+                </div>
               </div>
-              <div class="card-body pt-4 p-3">
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-md-6 mt-4">
+          <div class="card h-100 mb-4">
+            <div class="card-header pb-0 px-3">
+              <div class="row">
+                <div class="col-md-6">
+                  <h6 class="mb-0">Entrada</h6>
+                </div>
+                <div class="col-md-3">
+                  <a class="btn" href="cad_entrada.php">Cadastrar</a>
+                </div>
+              </div>
+            </div>
+            <div class="card-body pt-4 p-3">
               <?php
               require('conexao.php');
 
@@ -188,30 +161,30 @@
                       </div>
                     </div>
                     <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                    +R$<?php echo number_format((float)$dados['valor'], 2, ',', '.'); ?>
+                      +R$<?php echo number_format((float)$dados['valor'], 2, ',', '.'); ?>
                     </div>
                   </li>
                 </ul>
-                <?php } ?>
-              </div>
+              <?php } ?>
             </div>
           </div>
-          
-          <div class="col-md-6 mt-4">
-            <div class="card h-100 mb-4">
-              <div class="card-header pb-0 px-3">
-                <div class="row">
-                  <div class="col-md-6">
-                    <h6 class="mb-0">Saida</h6>
-                  </div>
-                  <div class="col-md-3">
-                    <a class="btn" href="cad_despesa.php">Cadastrar</a>
-                  </div>
+        </div>
+
+        <div class="col-md-6 mt-4">
+          <div class="card h-100 mb-4">
+            <div class="card-header pb-0 px-3">
+              <div class="row">
+                <div class="col-md-6">
+                  <h6 class="mb-0">Saida</h6>
+                </div>
+                <div class="col-md-3">
+                  <a class="btn" href="cad_despesa.php">Cadastrar</a>
                 </div>
               </div>
-              
-              <div class="card-body pt-4 p-3">
-                <?php
+            </div>
+
+            <div class="card-body pt-4 p-3">
+              <?php
               require('conexao.php');
 
               $query = "SELECT * FROM gastos";
@@ -230,98 +203,33 @@
                       </div>
                     </div>
                     <div class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
-                    -R$<?php echo number_format((float)$dados['valor'], 2, ',', '.'); ?>
+                      -R$<?php echo number_format((float)$dados['valor'], 2, ',', '.'); ?>
                     </div>
                   </li>
                 </ul>
-                <?php } ?>
-              </div>
+              <?php } ?>
             </div>
           </div>
         </div>
-        
       </div>
-  </main><footer class="footer pt-3  ">
-          <div class="container-fluid">
-            <div class="row align-items-center justify-content-lg-between">
-              <div class="col-lg-6 mb-lg-0 mb-4">
-                <div class="copyright text-center text-sm text-muted text-lg-start">
-                Copyright © <script>
-                    document.write(new Date().getFullYear())
-                  </script>,
-                  FiqueNoAzul Designed And Developed <i class="fa fa-heart"></i> by
-                  <a href="" class="font-weight-bold" target="_blank">Unastech</a>
-                </div>
-              </div>
-            </div>
+
+    </div>
+  </main>
+  <footer class="footer pt-3  ">
+    <div class="container-fluid">
+      <div class="row align-items-center justify-content-lg-between">
+        <div class="col-lg-6 mb-lg-0 mb-4">
+          <div class="copyright text-center text-sm text-muted text-lg-start">
+            Copyright © <script>
+              document.write(new Date().getFullYear())
+            </script>,
+            FiqueNoAzul Designed And Developed <i class="fa fa-heart"></i> by
+            <a href="" class="font-weight-bold" target="_blank">Unastech</a>
           </div>
-        </footer>
-  <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-      <i class="fa fa-cog py-2"> </i>
-    </a>
-    <div class="card shadow-lg ">
-      <div class="card-header pb-0 pt-3 ">
-        <div class="float-start">
-          <h5 class="mt-3 mb-0">Soft UI Configurator</h5>
-          <p>See our dashboard options.</p>
-        </div>
-        <div class="float-end mt-4">
-          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-            <i class="fa fa-close"></i>
-          </button>
-        </div>
-        <!-- End Toggle Button -->
-      </div>
-      <hr class="horizontal dark my-1">
-      <div class="card-body pt-sm-3 pt-0">
-        <!-- Sidebar Backgrounds -->
-        <div>
-          <h6 class="mb-0">Sidebar Colors</h6>
-        </div>
-        <a href="javascript:void(0)" class="switch-trigger background-color">
-          <div class="badge-colors my-2 text-start">
-            <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-          </div>
-        </a>
-        <!-- Sidenav Type -->
-        <div class="mt-3">
-          <h6 class="mb-0">Sidenav Type</h6>
-          <p class="text-sm">Choose between 2 different sidenav types.</p>
-        </div>
-        <div class="d-flex">
-          <button class="btn bg-gradient-primary w-100 px-3 mb-2 active" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-          <button class="btn bg-gradient-primary w-100 px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-        </div>
-        <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
-        <!-- Navbar Fixed -->
-        <div class="mt-3">
-          <h6 class="mb-0">Navbar Fixed</h6>
-        </div>
-        <div class="form-check form-switch ps-0">
-          <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-        </div>
-        <hr class="horizontal dark my-sm-4">
-        <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/soft-ui-dashboard-pro">Free Download</a>
-        <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/license/soft-ui-dashboard">View documentation</a>
-        <div class="w-100 text-center">
-          <a class="github-button" href="https://github.com/creativetimofficial/soft-ui-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/soft-ui-dashboard on GitHub">Star</a>
-          <h6 class="mt-3">Thank you for sharing!</h6>
-          <a href="https://twitter.com/intent/tweet?text=Check%20Soft%20UI%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fsoft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-          </a>
-          <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/soft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-          </a>
         </div>
       </div>
     </div>
-  </div>
+  </footer>
   <!--   Core JS Files   -->
   <script src="../assets/js/core/popper.min.js"></script>
   <script src="../assets/js/core/bootstrap.min.js"></script>
